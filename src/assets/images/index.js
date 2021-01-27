1d3e9469bd4013263ac4dccd19d10e2aec9ba35e
Edgar Lopez <edgarlopezmgs@live.com>
2021-01-27 17:33:01 -0600

    Login maquetado

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,11 +1,15 @@
 import Trofeo1 from "./trofeo1.svg"
 import Trofeo2 from "./trofeo2.svg"
 import Trofeo3 from "./trofeo3.svg"
+import Sonic4 from "./sonic4.gif"
+import Google from "./google.svg"
 
 
 const images = {
     Trofeo1,
     Trofeo2,
     Trofeo3,
+    Sonic4,
+    Google
 }
 export default images
